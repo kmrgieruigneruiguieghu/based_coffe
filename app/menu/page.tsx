c6e27ef4950631c465da181e106bff69c8b1b834
qqq2 <ilyaromensky@gmail.com>
2026-02-08 23:29:34 +0300

    main

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from 'next/link';
-import coffee_bg from '../public/coffee.webp';
 import time from '../public/time.jpg';
 import many_coffee from '../public/mnoga_coffee.jpg';
 import desert from '../public/desert.jpg';
@@ -8,25 +7,6 @@ import desert from '../public/desert.jpg';
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#43302b] to-[#805A3C]">
-      <section className="relative min-h-[830px] flex items-center text-white overflow-hidden">
-        <div className="absolute inset-0">
-          <Image src={coffee_bg} alt="." fill className="object-cover" sizes="100vw"
-            style={{ objectPosition: 'center center' }}/>
-        </div>
-        <div className="absolute inset-0 bg-gradient-to-r from-black/100 via-black/70 to-transparent"></div>
-        <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full">
-          <div className="max-w-2xl">
-            <h1 className="text-4xl md:text-6xl font-bold mb-6">
-              Кофейня <span className="text-[#D7D683]">Based Coffee</span>
-            </h1>
-            <p className="text-xl mb-8">
-              Лучшая кофейня которую ты когда-либо видел. <br/> Лучше уже не будет</p>
-            <Link href="/menu" className="inline-flex items-center bg-[#A64B00] hover:bg-[#FF7400] text-white
-              font-bold py-3 px-8 rounded-full text-lg transition-all duration-300 transform hover:scale-105">
-              Посмотреть меню</Link>
-          </div>
-        </div>
-      </section>
       <section className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-3xl font-bold text-center mb-12">Наша кофейня лучше чем вы думаете и вот почему</h2>
@@ -59,16 +39,6 @@ export default function Home() {
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section className="text-white py-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
-          <h2 className="text-3xl font-bold mb-6">Интерактив "Бариста"</h2>
-          <p className="text-xl mb-8 max-w-2xl mx-auto">
-            Побудь на пол часа баристой за 500 рублей и получишь незабываемый опыт))) </p>
-          <Link href="/contacts" className="inline-flex items-center bg-[#A64B00] hover:bg-[#FF7400] text-white
-            font-bold py-3 px-8 rounded-full text-lg transition-all duration-300 transform hover:scale-105">
-            Связаться для уточнения</Link>
         </div>
       </section>
     </div>
